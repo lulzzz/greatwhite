2c9e12f864262b4d17b1a05f0b03acc8c903e83c
Brian Lance <brianrlance@gmail.com>
2017-02-23 13:59:40 -0800

    demo to sam

---
+++ R/doc_data.R
@@ -0,0 +1,14 @@
+#' Inventory Details (raw)
+#'
+#' A dataset containing the inventory quantities for Culk
+#' products, as exported from TradeGecko with no modifications.
+#'
+#' @format A data frame with 351 rows and 8 variables:
+#' \describe{
+#'   \item{Product}{product name}
+#'   \item{Variant}{variant name}
+#'   \item{SKU}{stock keeping unit}
+#'   ...
+#' }
+#' @source \url{https://go.tradegecko.com/intelligence/inventory_detail.csv?order=product_name%20asc&}
+"inventory_details_raw"
